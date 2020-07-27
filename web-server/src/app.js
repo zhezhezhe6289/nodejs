711f0bd95edc3daa872381f533ec9f3e476b436f
eugenechua <zhe1217_92@hotmail.com>
2020-07-27 20:09:24 +0800

    handlebars.js(hbs) added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,46 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialPath = path.join(__dirname, '../templates/partials')
+
+// setup handlebar machine
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialPath)
+
+// setup static directory serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('/weather', (req, res) => {
-    res.send([
-        {
-            forecast: 'its currently raining',
-            location: 'New Taipei'
-        }
-    ])
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Eugene'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'Weather App',
+        name: 'Eugene'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Weather App',
+        name: 'Eugene',
+        helpText: 'This is a helpful text!'
+    })
 })
 
 app.listen(3000, '127.0.0.1', () => {
-    console.log('伺服器已啟動 port 為 3000' + 3000)
+    console.log('伺服器已啟動 port 為 3000')
 })
 
 
