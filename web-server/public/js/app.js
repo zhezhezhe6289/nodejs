7886f65f9b2cb8c53dc2c54bf5d1d94256f11648
eugenechua <zhe1217_92@hotmail.com>
2020-07-28 20:55:53 +0800

    added javascript fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,4 +1,11 @@
-const url = 'http://puzzle.mead.io/puzzle'
+const url = 'http://localhost:3000/weather?address=boston'
 fetch(url).then((response) => {
-    console.log(response)
+    response.json().then((data) => {
+        try {
+            console.log(data.location)
+            console.log(data.forecast)
+        } catch (error) {
+            console.log(data.error)
+        }
+    })
 })
